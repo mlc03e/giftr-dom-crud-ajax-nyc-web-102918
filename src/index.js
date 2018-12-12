c5808d7be4c1414779243657cd84292cc6f6c199
gavinmoore36 <gavizoid16@gmail.com>
2018-12-12 11:00:08 -0500

    some changes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM has been fully loaded')
   console.table(gifts)
   //Hello Meghan
+
+  const form = document.querySelector('.ui.icon.input')
+  form.addEventListener('text', function (event) {
+    event.preventDefault()
+  console.log(event.target)
+  const input = document.querySelector('#filter-input').value
+  console.log(input)
+})
 })
