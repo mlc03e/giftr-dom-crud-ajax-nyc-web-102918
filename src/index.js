fe821174dc67e6858bb018e5e0f966d1772618dc
gavinmoore36 <gavizoid16@gmail.com>
2018-12-12 11:51:32 -0500

    gift form changes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM has been fully loaded')
   console.table(gifts)
   //Hello Meghan
+  const giftList = document.querySelector('.gift-list')
+  const form = document.querySelector('#new-gift-form')
+    form.addEventListener('submit', function () {
+      event.preventDefault()
 
-  const form = document.querySelector('.ui.icon.input')
-  form.addEventListener('text', function (event) {
-    event.preventDefault()
-  console.log(event.target)
-  const input = document.querySelector('#filter-input').value
-  console.log(input)
-})
+      const newGift = event.target.querySelector('#gift-name-input').value
+      const newImg = event.target.querySelector('#gift-image-input').value
+      const newLiTag = document.createElement('li')
+      newLiTag.textContent = newGift
+      newLiTag.urlContent = newImg
+      giftList.appendChild(newLiTag)
+    })
+//   const form = document.querySelector('.ui.icon.input')
+//   form.addEventListener('keypress', function (event) {
+//     var key = event.which || event.keyCode;
+//     if (key === 13) {} // 13 is enter apparently // code for enter
+//   console.log(event.target)
+//   const input = document.querySelector('#filter-input').value
+//   console.log(input)
+// })
 })
